fb98d5291446f165e5185d08b03fca4832fd7ca7
Frederic <zetachow@163.com>
2021-04-04 16:48:53 +0800

    实现跳过过快的帧

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let ws_minicap
 let ws_minitouch
 let ws_whatsinput
 const localhost = "0.0.0.0"
+let screenSending = false
 
 console.log("ServerAddress:", process.argv[2])
 let severAddr = process.argv[2]
@@ -31,7 +32,12 @@ function start_transmit(ws_serv_initiative) {
         console.log("minicap open")
     };
     ws_minicap.onmessage = (msg) => {
-        ws_serv_initiative.send(msg.data)
+        if (screenSending == false) {
+            ws_serv_initiative.send(msg.data)
+            if (typeof msg.data == "object") {
+                screenSending = true;
+            }
+        }
     };
     ws_minicap.onerror = (ev) => {
         ws_minicap.close()
@@ -80,7 +86,7 @@ function start_transmit(ws_serv_initiative) {
 ////////////////////////////////////////
 let started = false
 ws_serv_initiative.onopen = (ev) => {
-    // console.log("serv ctrl open")
+    console.log("serv ctrl open")
     //读取设备信息，并且发送给服务器
     request(`http://${localhost}:7912/info`, {
         json: false
@@ -97,10 +103,13 @@ ws_serv_initiative.onmessage = (msg) => {
     console.log("receive from server:", msg.data)
     if (msg.data == "initiative_start" && !started) {
         started = start_transmit(ws_serv_initiative)
+    } else if (msg.data == "screen_received") {
+        screenSending = false;
     } else if (msg.data.indexOf("minitouch:") == 0 && started && ws_minitouch.readyState == 1) {
         ws_minitouch.send(msg.data.substr("minitouch:".length))
     } else if (msg.data.indexOf("whatsinput:") == 0 && started && ws_whatsinput.readyState == 1) {
         ws_whatsinput.send(msg.data.substr("whatsinput:".length))
+        console.log(msg.data.substr("whatsinput:".length))
     } else if (msg.data.indexOf("shell:") == 0 && started) {
         request(`http://${localhost}:7912/shell?command= ${msg.data.substr("shell:".length)}`, {
             json: false
@@ -117,6 +126,7 @@ ws_serv_initiative.onmessage = (msg) => {
         ws_minicap && ws_minicap.close()
         ws_minitouch && ws_minitouch.close()
         ws_whatsinput && ws_whatsinput.close()
+        console.log("all stoped")
     }
 };
 ws_serv_initiative.onerror = (ev) => {
@@ -132,5 +142,6 @@ ws_serv_initiative.onclose = (ev) => {
     process.exit(1)
 };
 
-
+//todo:
 // 改用scrcpy 同屏和操作
+// 画面延迟，因为websocket传输画面太慢
