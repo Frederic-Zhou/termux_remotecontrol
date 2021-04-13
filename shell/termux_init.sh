da2dee5a811de860e5be20b2638f57ba037a24d9
Frederic <zetachow@163.com>
2021-04-14 00:14:50 +0800

    优化脚本

--- shell/termux_init.sh
+++ shell/termux_init.sh
@@ -45,15 +45,9 @@ adb -t 1 push ~/termux_remotecontrol/app/atx-agent /data/local/tmp/atx-agent
 adb -t 1 shell chmod 755 /data/local/tmp/atx-agent
 adb -t 1 shell /data/local/tmp/atx-agent server -d
 echo -e "\033[32m ===ATX-agent INSTALL OVER=== \033[0m"
-
-result=$(ps -ef | grep scrcpy-server)
-if [[ "$result" =~ "scrcpy-server.jar" ]]; then
-    echo -e "\033[33m scrcpy-server.jar started \033[0m"
-else
-    #adb -t 1 :第一个连接上的设备
-    adb -t 1 push ~/termux_remotecontrol/app/scrcpy-server.jar /data/local/tmp/scrcpy-server.jar
-    adb -t 1 shell CLASSPATH=/data/local/tmp/scrcpy-server.jar nohup app_process scrcpy-server.jar com.genymobile.scrcpy.Server 1.17-ws1 web 8886 2>&1 >/dev/null &
-fi
+#adb -t 1 :第一个连接上的设备
+adb -t 1 push ~/termux_remotecontrol/app/scrcpy-server.jar /data/local/tmp/scrcpy-server.jar
+adb -t 1 shell CLASSPATH=/data/local/tmp/scrcpy-server.jar nohup app_process scrcpy-server.jar com.genymobile.scrcpy.Server 1.17-ws1 web 8886 2>&1 >/dev/null &
 echo -e "\033[32m ===scrcpy-server.jar INSTALL OVER=== \033[0m"
 
 #######################################################
